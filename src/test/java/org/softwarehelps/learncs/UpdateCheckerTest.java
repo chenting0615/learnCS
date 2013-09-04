6767d6b39048b709dbac2865db1a745fa5953793
Peter Dobson <pdobson@softwarehelps.com>
2013-09-03 17:08:00 -0700

    Test compareVersions() method

---
+++ src/test/java/org/softwarehelps/learncs/UpdateCheckerTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.softwarehelps.learncs;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Peter Dobson
+ */
+public class UpdateCheckerTest extends TestCase {
+    
+    UpdateChecker instance;
+    
+    public UpdateCheckerTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        instance = new UpdateChecker(null, 
+                "http://softwarehelps.org/download/learnCS.version",
+                "http://softwarehelps.org/wiki/index.php?title="
+                + "Download_CIS_5_Lab_Software");
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testGetNewVersion() {
+        System.out.println("getNewVersion");
+        String expResult = "1.0.1";
+        String result = instance.getNewVersion();
+        assertEquals(expResult, result);
+    }
+
+    public void testCompareVersions() {
+        System.out.println("compareVersions");
+        String a = "1.0.1";
+        String b = "1.0.1";
+        int expResult = 0;
+        int result = instance.compareVersions(a, b);
+        assertEquals(expResult, result);
+        b = "1.0.0";        
+        result = instance.compareVersions(a, b);
+        assertTrue(result > 0);
+        b = "1.0.2";        
+        result = instance.compareVersions(a, b);
+        assertTrue(result < 0);
+        b = "1.0-ALPHA";
+    }    
+}
