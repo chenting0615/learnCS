8047122a5f4bde23ad50b59600769f6e8b88c1cc
Peter Dobson <pdobson@softwarehelps.com>
2013-09-03 17:09:50 -0700

    Start UpdateChecker thread to check if current running software is latest version.  Thread exits application using System.exit(0) if the user elects to download a new version of the software.

--- src/main/java/org/softwarehelps/learncs/MainWindow.java
+++ src/main/java/org/softwarehelps/learncs/MainWindow.java
@@ -9,9 +9,13 @@ import javax.swing.JFrame;
  * Display SelectApplet JPanel in a scrolling window.
  */
 public class MainWindow extends JFrame
-{
+{    
     MainWindow() {
-        
+ 
+        new UpdateChecker(this, 
+                "http://softwarehelps.org/download/learnCS.version",
+                "http://softwarehelps.org/wiki/index.php?title="
+                + "Download_CIS_5_Lab_Software").start();
         setTitle("Explorations Applet Launcher");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(750, 500));
